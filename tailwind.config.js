f64720808cef88780a5474e9a5d54085367a2554
WhitWander <vcmikhaelcp@gmail.com>
2024-02-24 17:40:16 -0300

    upv2

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'dragon': "url('dist/images/fundo.png')",
+      }
+    },
   },
   plugins: [],
 }
